73693932b28d602a1f58f15cb048dcd84b61b31e
Misha2007 <mishadrogovoz@gmail.com>
2025-06-01 00:17:07 +0300

    Added port to the database

--- workout_app_backend/util/db.js
+++ workout_app_backend/util/db.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_PASSWORD,
   {
     host: process.env.DATABASE_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DATABASE_DIALECT,
   }
 );
